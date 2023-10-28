b1148d2c6a09af19d483f06ae290f732cc834825
Tal Jacob <taljacob2@gmail.com>
2023-10-28 16:28:56 +0300

    Add Models
    Signed-off-by: Tal Jacob <taljacob2@gmail.com>

---
+++ src/models/neighborhood.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+
+
+const NeighborhoodSchema = mongoose.Schema({
+    name: {
+        type: String,
+        unique: true,
+        required: [true, 'Missing name.'],
+        minLength: [2, 'Name must be minimum 2 chars.'],
+        maxLength: [50, 'Name can\'t exceed 50 chars.'],
+        validate: {
+            validator: (value) => {
+                return value.indexOf('  ') === -1;
+            },
+            message: 'Name can\'t contain multiple spaces.'
+        }
+    },
+    geometry: {
+        type: {
+            type: String,
+            enum: ['Polygon'], // 'type' must be 'Polygon'
+            required: true
+        },
+        coordinates: {
+            type: [Number],
+            required: true,
+            validate: {
+                validator: (value) => {
+                    return value.length >= 1
+                },
+                message: 'Coordinates must have at least 1 element.'
+            }
+        }
+    }
+}, {
+    versionKey: false,
+    toJSON: { virtuals: true },
+    id: false
+});
+
+// class name, schema, collection
+const NeighborhoodModel = mongoose.model('NeighborhoodModel', NeighborhoodSchema, 'neighborhoods');
+
+
+export default NeighborhoodModel;
---
+++ src/models/restaurant.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+
+
+const RestaurantSchema = mongoose.Schema({
+    name: {
+        type: String,
+        unique: true,
+        required: [true, 'Missing name.'],
+        minLength: [2, 'Name must be minimum 2 chars.'],
+        maxLength: [50, 'Name can\'t exceed 50 chars.'],
+        validate: {
+            validator: (value) => {
+                return value.indexOf('  ') === -1;
+            },
+            message: 'Name can\'t contain multiple spaces.'
+        }
+    },
+    location: {
+        type: {
+            type: String,
+            enum: ['Point'], // 'type' must be 'Point'
+            required: true
+        },
+        coordinates: {
+            type: [Number],
+            required: true,
+            validate: {
+                validator: (value) => {
+                    return value.length === 2
+                },
+                message: 'Coordinates must have exactly 2 elements.'
+            }
+        }
+    }
+}, {
+    versionKey: false,
+    toJSON: { virtuals: true },
+    id: false
+});
+
+// class name, schema, collection
+const RestaurantModel = mongoose.model('RestaurantModel', RestaurantSchema, 'restaurants');
+
+
+export default RestaurantModel;
