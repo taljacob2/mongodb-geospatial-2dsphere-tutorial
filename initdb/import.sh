d5c076f604e4e622bf5f17759f268bfd82bb57be
Tal Jacob <taljacob2@gmail.com>
2023-10-28 09:06:58 +0300

    Change Mongo Import To Use Default BSON Json Exports
    Signed-off-by: Tal Jacob <taljacob2@gmail.com>

--- initdb/import.sh
+++ initdb/import.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-mongoimport $MONGO_URI -c restaurants --file /initdb/restaurants.json --jsonArray
-mongoimport $MONGO_URI -c neighborhoods --file /initdb/neighborhoods.json --jsonArray
+mongoimport $MONGO_URI -c restaurants --file /initdb/restaurants.json
+mongoimport $MONGO_URI -c neighborhoods --file /initdb/neighborhoods.json
