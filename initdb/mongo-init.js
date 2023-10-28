2b9e038f12e9cf95923ab010c10fb081d00b8792
Tal Jacob <taljacob2@gmail.com>
2023-10-28 15:13:37 +0300

    `DB_DATABASE` Environment Variable Dynamically In `mongo-init.js`
    Signed-off-by: Tal Jacob <taljacob2@gmail.com>

--- initdb/mongo-init.js
+++ initdb/mongo-init.js
@@ -1,6 +1,9 @@
 conn = new Mongo()
-db = conn.getDB("geospatialTutorialDb")
+db = conn.getDB(`${process.env.DB_DATABASE}`)
 
+
+// TODO: DEBUG
+console.log("process.env", process.env)
 
 db.restaurants.createIndex({ location: "2dsphere" })
 db.neighborhoods.createIndex({ geometry: "2dsphere" })
