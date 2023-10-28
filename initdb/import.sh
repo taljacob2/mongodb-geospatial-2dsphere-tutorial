b909114617ce90b4008b9ed92deb8e7ddd0c3cd4
Tal Jacob <taljacob2@gmail.com>
2023-10-28 08:54:20 +0300

    Add `mongo_import` To Import `neighborhoods` Collection
    Signed-off-by: Tal Jacob <taljacob2@gmail.com>

--- initdb/import.sh
+++ initdb/import.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 mongoimport $MONGO_URI -c restaurants --file /initdb/restaurants.json --jsonArray
+mongoimport $MONGO_URI -c neighborhoods --file /initdb/neighborhoods.json --jsonArray
