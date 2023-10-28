8658ceda5f94ccc790c3c75ca3b7a5f882561e56
Tal Jacob <taljacob2@gmail.com>
2023-10-28 16:47:36 +0300

    Fix Dal Connection String And Env
    Signed-off-by: Tal Jacob <taljacob2@gmail.com>

--- src/data-access/dal.js
+++ src/data-access/dal.js
@@ -4,9 +4,9 @@ const mongoose = require('mongoose');
 const connectAsync = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URI);
-        logger.info(`MongoDB is connected to DB ${process.env.DB_DATABASE}`);
+        console.log(`MongoDB is connected to DB ${process.env.DB_DATABASE}`);
     } catch (err) {
-        logger.error(err.message);
+        console.error(err.message);
         process.exit(1);
     }
 };
