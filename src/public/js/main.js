2d90e0fc79c93c83d5fd9499719197a74b6bfb3d
Tal Jacob <taljacob2@gmail.com>
2023-10-29 23:02:02 +0200

    Reorder `initMap` With Default Values
    Signed-off-by: Tal Jacob <taljacob2@gmail.com>

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -11,24 +11,30 @@
      */
     let map;
 
-    /**
-     * Initializes Google Map.
-     */
-    const initMap = () => {
-        map = new google.maps.Map(document.getElementById("map"), {
-            zoom: 2,
-            center: new google.maps.LatLng(2.8, -187.3),
-            mapTypeId: "terrain",
-        });
+   /**
+    * Initializes Google Map.
+    * 
+    * @param {*} mapOptions 
+    */
+    const initMap = ({
+        zoom = 2,
+        center = new google.maps.LatLng(2.8, -187.3),
+        mapTypeId = "terrain"
+    }) => {
+        const mapOptions = { zoom, center, mapTypeId }
+        map = new google.maps.Map(document.getElementById("map"), mapOptions);
     }
 
     /**
      * Loop through the restaurants array and place a marker for each set of
-     * coordinates.
+     * coordinates, where each document is a `Point`.
      * 
      * @param {*} restaurants Restaurants to present in map.
+     * @see https://developers.google.com/maps/documentation/javascript/importing_data
      */
     const setRestaurantsMarkersInMap = (restaurants) => {
+        let returnedLatLng;
+
         for (let i = 0; i < restaurants.length; i++) {
             const coords = restaurants[i].location.coordinates;
             const latLng = new google.maps.LatLng(coords[1], coords[0]);
@@ -37,42 +43,73 @@
                 position: latLng,
                 map: map,
             });
+
+            returnedLatLng = latLng;
         }
+
+        return returnedLatLng;
     };
 
     /**
      * Loop through the neighborhoods array and place a marker for each set of
-     * coordinates.
+     * coordinates, where each document is a `Polygon` with holes.
      * 
      * @param {*} neighborhoods Neighborhoods to present in map.
+     * @see https://developers.google.com/maps/documentation/javascript/examples/polygon-hole
      */
     const setNeighborhoodsMarkersInMap = (neighborhoods) => {
-        for (let i = 0; i < neighborhoods.length; i++) {
-            console.log(JSON.stringify(neighborhoods[i], null, 2));
-            const coords = neighborhoods[i].geometry.coordinates[0][0];
-            const latLng = new google.maps.LatLng(coords[1], coords[0]);
+        let returnedLatLng;
 
-            new google.maps.Marker({
-                position: latLng,
-                map: map,
+        for (let i = 0; i < neighborhoods.length; i++) {
+            const outerCoordsRaw = neighborhoods[i].geometry.coordinates[0];
+            let outerCoords = []
+            for(let j = 0; j < outerCoordsRaw.length; j++) {
+                const latLng = new google.maps.LatLng(outerCoordsRaw[j][1], outerCoordsRaw[j][0]);
+                outerCoords[j] = latLng;
+            }
+            const innerCoordsRaw = neighborhoods[i].geometry.coordinates[0];
+            let innerCoords = []
+            for(let j = 0; j < innerCoordsRaw.length; j++) {
+                const latLng = new google.maps.LatLng(innerCoordsRaw[j][1], innerCoordsRaw[j][0]);
+                innerCoords[j] = latLng;
+            }
+            const polygon = new google.maps.Polygon({
+                paths: [outerCoords, innerCoords],
+                strokeColor: "#FF0000",
+                strokeOpacity: 0.8,
+                strokeWeight: 2,
+                fillColor: "#FF0000",
+                fillOpacity: 0.35,
             });
+
+            returnedLatLng = outerCoords;
+    
+            polygon.setMap(map);
         }
+
+        return returnedLatLng;
     };
 
     const pageLoader = () => {
         if ($('.import-json-restaurants-page').length) {
             $.get(`${HOST}/api/restaurant`)
                 .done((data, textStatus, jqXHR) => {
-                    initMap();
+                    initMap({
+                        zoom: 18,
+                        center: setRestaurantsMarkersInMap(data)
+                    });
                     setRestaurantsMarkersInMap(data);
                 })
         } else if ($('.import-json-neighborhoods-page').length) {
             $.get(`${HOST}/api/neighborhood`)
                 .done((data, textStatus, jqXHR) => {
-                    initMap();
+                    initMap({
+                        zoom: 12,
+                        center: setNeighborhoodsMarkersInMap(data)
+                    });
                     setNeighborhoodsMarkersInMap(data);
                 })
-        } 
+        }
     }
     pageLoader();
 
