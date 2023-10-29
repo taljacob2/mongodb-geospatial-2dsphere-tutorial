b4ce80bc44e155dfac951a113a2619d7eb90b187
Tal Jacob <taljacob2@gmail.com>
2023-10-29 16:46:43 +0200

    Load Restaurants MongoDB Collection To Google Maps
    Signed-off-by: Tal Jacob <taljacob2@gmail.com>

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -2,16 +2,53 @@
     "use strict";
     console.clear();
 
-    pageLoader();
-    function pageLoader() {
+    /**
+     * Stores to google map.
+     * 
+     * It is required for `initMap` to work properly.
+     * 
+     * @see initMap
+     */
+    let map;
+
+    /**
+     * Initializes Google Map.
+     */
+    const initMap = () => {
+        map = new google.maps.Map(document.getElementById("map"), {
+            zoom: 2,
+            center: new google.maps.LatLng(2.8, -187.3),
+            mapTypeId: "terrain",
+        });
+    }
+
+    /**
+     * Loop through the results array and place a marker for each set of
+     * coordinates.
+     * 
+     * @param {*} documents documents to present in map.
+     */
+    const setMarkersInMap = (documents) => {
+        for (let i = 0; i < documents.length; i++) {
+            const coords = documents[i].location.coordinates;
+            const latLng = new google.maps.LatLng(coords[1], coords[0]);
+
+            new google.maps.Marker({
+                position: latLng,
+                map: map,
+            });
+        }
+    };
+
+    const pageLoader = () => {
         if ($('.import-json-restaurants-page').length) {
-
             $.get(`${HOST}/api/restaurant`)
-            .done(function(data, textStatus, jqXHR) {
-                console.log(data);
-            })
-
+                .done((data, textStatus, jqXHR) => {
+                    initMap();
+                    setMarkersInMap(data);
+                })
         }
     }
+    pageLoader();
 
 }(jQuery));
