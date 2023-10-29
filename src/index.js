262b8733c1267ea7d274044ff425cddc55424bab
Tal Jacob <taljacob2@gmail.com>
2023-10-29 16:13:20 +0200

    Add Cors
    Signed-off-by: Tal Jacob <taljacob2@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors')
 const dotenv = require('dotenv');
 const dotenvExpand = require('dotenv-expand');
 const restaurantController = require('./controllers/restaurant-controller');
@@ -13,8 +14,9 @@ dotenvExpand.expand(dotenv.config());
 
 const app = express();
 const PORT = process.env.NODE_PORT;
-
+app.use(cors());
 app.use(express.json());
+
 app.use('/api/restaurant', restaurantController);
 app.use('/api/neighborhood', neighborhoodController);
 createPublicRoute(app);
