dc35eb14cd05ffa4db66233e54d56b82a6e581aa
Tal Jacob <taljacob2@gmail.com>
2023-10-30 12:14:00 +0200

    Fix `setNeighborhoodsMarkersInMap`
    Signed-off-by: Tal Jacob <taljacob2@gmail.com>

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -26,7 +26,9 @@
     }
 
     /**
-     * Loop through the restaurants array and place a marker for each set of
+     * Set all the restaurants as Google Map markers.
+     * 
+     * Loops through the restaurants array and place a marker for each set of
      * coordinates, where each document is a `Point`.
      * 
      * @param {*} restaurants Restaurants to present in map.
@@ -51,7 +53,8 @@
     };
 
     /**
-     * Places a polygon marker on a Google Map.
+     * Places a polygon marker on a Google Map, where each geoJson type is a
+     * `Polygon`.
      * 
      * @param {*} geoJson geoJson that its 'type' is 'Polygon'.
      * @returns {google.maps.LatLng}
@@ -74,57 +77,63 @@
             fillColor: "#FF0000",
             fillOpacity: 0.35,
         });
-
         polygon.setMap(map);
-
-        // TODO: debug
-        console.log(outerCoords);
 
         return outerCoords[0];
     }
 
     /**
-     * Loop through the neighborhoods array and place a marker for each set of
-     * coordinates, where each document is a `Polygon` with holes.
+     * Places a polygon marker on a Google Map, where each geoJson type is
+     * 'MultiPolygon', i.e. `Polygon` with holes.
      * 
-     * @param {*} neighborhoods Neighborhoods to present in map.
+     * @param {*} geoJson geoJson that its 'type' is 'MultiPolygon'.
+     * @returns {google.maps.LatLng}
      * @see https://developers.google.com/maps/documentation/javascript/examples/polygon-hole
+     */
+    const setMultiPolygonMarkersInMap = (geoJson) => {
+        if (geoJson.type != 'MultiPolygon') { return null; }
+        if (!geoJson.coordinates) { return null; }
+
+        
+        let outerCoords = []
+        const outerCoordsRaw = geoJson.coordinates[0][0];
+        for (let i = 0; i < outerCoordsRaw.length; i++) {
+            const latLng = new google.maps.LatLng(outerCoordsRaw[i][1], outerCoordsRaw[i][0]);
+            outerCoords[i] = latLng;
+        }
+
+        let innerCoords = []
+        const innerCoordsRaw = geoJson.coordinates[1][0];
+        for (let i = 0; i < innerCoordsRaw.length; i++) {
+            const latLng = new google.maps.LatLng(innerCoordsRaw[i][1], innerCoordsRaw[i][0]);
+            outerCoords[i] = latLng;
+        }
+
+        const polygon = new google.maps.Polygon({
+            paths: [outerCoords, innerCoords],
+            strokeColor: "#FF0000",
+            strokeOpacity: 0.8,
+            strokeWeight: 2,
+            fillColor: "#FF0000",
+            fillOpacity: 0.35,
+        });
+        polygon.setMap(map);
+        
+        return outerCoords[0];
+    }
+
+    /**
+     * Set all the neighborhoods as Google Map markers.
+     * @param {*} neighborhoods Neighborhoods to present in map.
      */
     const setNeighborhoodsMarkersInMap = (neighborhoods) => {
         let returnedLatLng;
 
         for (let i = 0; i < neighborhoods.length; i++) {
             const neighborhoodGeoJson = neighborhoods[i].geometry;
-            let outerCoords = []
-            let innerCoords = []
 
             returnedLatLng = setPolygonMarkersInMap(neighborhoodGeoJson) || returnedLatLng;
-            // const outerCoordsRaw = neighborhoods[i].geometry.coordinates[0];
-            // for (let i = 0; i < outerCoordsRaw.length; i++) {
-            //     const latLng = new google.maps.LatLng(outerCoordsRaw[i][1], outerCoordsRaw[i][0]);
-            //     outerCoords[i] = latLng;
-            // }
-            
-            // // if (neighborhoods[i].geometry.coordinates.length >= 2) {
-            // //     const innerCoordsRaw = neighborhoods[i].geometry.coordinates[1];
-            // //     for (let i = 0; i < innerCoordsRaw.length; i++) {
-            // //         const latLng = new google.maps.LatLng(innerCoordsRaw[i][1], innerCoordsRaw[i][0]);
-            // //         innerCoords[i] = latLng;
-            // //     }
-            // // }
-
-            // const polygon = new google.maps.Polygon({
-            //     paths: [outerCoords, innerCoords],
-            //     strokeColor: "#FF0000",
-            //     strokeOpacity: 0.8,
-            //     strokeWeight: 2,
-            //     fillColor: "#FF0000",
-            //     fillOpacity: 0.35,
-            // });
-
-            // returnedLatLng = outerCoords;
-    
-            // polygon.setMap(map);
+            returnedLatLng = setMultiPolygonMarkersInMap(neighborhoodGeoJson) || returnedLatLng;           
         }
 
         return returnedLatLng;
